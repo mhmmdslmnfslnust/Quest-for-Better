fbc26228590e2840fa8e935926d2344d857c2cc8
Muhammad Suleman Faisal <mfaisal.bese23seecs@student.nust.edu.pk>
2025-08-11 22:39:44 +0500

    Re purpose

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:3001/:path*', // Proxy to Backend
+        destination: 'http://localhost:3001/api/:path*', // Proxy to Backend
       },
     ]
   },
